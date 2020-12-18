a5f81f89a627bc6d700cad805d56db06f78c38ed
Hampus Backman <hampus@safetyamp.com>
2020-12-18 08:40:19 -0500

    Day 13 part B

--- app/Y2020/Day13.php
+++ app/Y2020/Day13.php
@@ -25,5 +25,48 @@ class Day13
 
 	public static function B($input)
 	{
+    $schedules = explode(PHP_EOL, $input)[1];
+    $schedules = explode(',', $schedules);
+
+	  $inc = (int)$schedules[0];
+	  $cur = 0;
+
+	  for ($t = 1; $t < count($schedules); $t++) {
+	    // There's no restrictions of "x" schedules
+	    if ($schedules[$t] == 'x') {
+	      continue;
+      }
+
+	    $first = 0;
+
+	    while (true) {
+	      $bus = (int)$schedules[$t];
+	      // If the carry + the position is divisible
+        // by the schedule, then we might've found
+        // our answer.
+	      if ( ($cur + $t) % $bus == 0) {
+
+	        if ($first == 0) {
+	          // If we've reached the loop limit, then
+            // we have found our answer.
+	          if ($t == count($schedules) - 1) {
+	            break;
+            }
+
+	          // Our first value becomes the current
+            // incremental value.
+	          $first = $cur;
+          }
+	        else {
+	          $inc = $cur - $first;
+	          break;
+          }
+        }
+
+	      $cur += $inc;
+      }
+    }
+
+	  return $cur;
 	}
 }
